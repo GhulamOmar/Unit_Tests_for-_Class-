fbccabe73cf6f2d42fbc6378c92f4858cf03a7bc
Omar <gomar@dmacc.edu>
2019-10-29 17:30:05 -0500

    final test

---
+++ class_definitions/driver.py
@@ -0,0 +1,4 @@
+from class_definitions.student_class import Student
+"""Driver"""
+student = Student('Omar','Ghulam','BIS',60.0)
+print(student)
--- class_definitions/test_student_class.py
+++ class_definitions/test_student_class.py
@@ -2,6 +2,16 @@ import unittest
 
 from class_definitions import student_class as _student
 
+"""" test.py for student_class.py"""
+
+
+def test_inital_all_attributes():
+    student1 = _student.Student('Omar', 'Ghulam', 'BIS', 60.0)
+    assert student1.last_name == 'Omar'
+    assert student1.first_name == 'Ghulam'
+    assert student1.major == 'BIS'
+    assert student1.gpa == 60.0
+
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
@@ -14,31 +24,31 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.studen.last_name, 'Omar')
         self.assertEqual(self.studen.first_name, 'Ghulam')
 
-    def test_inital_all_attributes(self):
-        student1 = _student.Student('Omar', 'Ghulam', 'BIS', 60.0)  # this is not self.person from setUp, but local
-        assert student1.last_name == 'Omar'  # note no self here on person or assert
-        assert student1.first_name == 'Ghulam'
-        assert student1.major == 'BIS'
-        assert student1.gpa == 60.0
-
-    def test_object_not_created_error_last_name(self):
+    def test_not_created_error_last_name(self):
         with self.assertRaises(ValueError):
             name = _student.Student('555', 'Ghulam', "BIS", 55.0)
 
-    def test_object_not_created_error_fist_name(self):
+    def test_created_error_fist_name(self):
         with self.assertRaises(ValueError):
-            p = _student.Student('Omar', '333', "BIS", 65.0)
+            name = _student.Student('Omar', '333', "BIS", 65.0)
 
-    def test_object_not_created_error_major(self):
+    def test_created_error_major(self):
         with self.assertRaises(ValueError):
             name = _student.Student('Omar', 'Ghulam', '55', 66.5)
 
-    def test_person_class_display_name_gpa(self):
-        name = _student.Student('Omar', 'Ghulam', 'BIS', 54.0)  # Does not use person from setUp(), has local p
+    def test_class_name_gpa(self):
+        name = _student.Student('Omar', 'Ghulam', 'BIS', 54.0)
         self.assertEqual('Omar,Ghulam', 'Omar,Ghulam')
 
-    def test_initial_value_required_attributes1(self):
+    def test_isinstance_gpa_float(self):
         self.assertEqual(self.studen.gpa, 60.0)
+
+    def test_craete_firstname(self):
+        self.assertEqual(self.studen.first_name, 'Ghulam')
+
+    def test_craete_laststname(self):
+        self.assertEqual(self.studen.last_name, 'Omar')
+
 
 if __name__ == '__main__':
     unittest.main()
